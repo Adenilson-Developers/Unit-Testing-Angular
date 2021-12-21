2670c67194c250f2afa384afcbd3763104b0db61
Adenilson-Developers <adenilson.developers@gmail.com>
2021-12-21 07:53:22 -0300

    add todo html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Todo } from './interface/todo-models';
+import { TodoService } from './shared/services/todo.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Unit-test-app';
+  title = 'Unit-test-app'
+
+  constructor(private todoService: TodoService){}
+
+  subscriptions: Subscription[] = [];
+  todos: Todo[] = [];
+
+  ngOnInit(){
+    this.todoService.getTodos().subscribe({
+      next: todos => this.todos = todos,
+      error: err => {}
+    })
+  }
 }
--- src/app/shared/services/todo.service.spec.ts
+++ src/app/shared/services/todo.service.spec.ts
@@ -1,16 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async, inject } from '@angular/core/testing';
+import { of } from 'rxjs';
 import { TodoService } from './todo.service';
 
-describe('Service: Todo', () => {
+describe('TodoService', () => {
+  let service: TodoService;
+  const httpStub = {
+    get: (_params: any) => of([
+      {
+        "userId": 1,
+        "id": 1,
+        "title": "delectus out autem",
+        "completed": false
+      }
+    ])
+  }
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [TodoService]
+      providers: [
+        {
+          provide: HttpClient,
+          useValue: httpStub
+        }
+      ]
     });
+    service = TestBed.inject(TodoService);
   });
 
-  it('should ...', inject([TodoService], (service: TodoService) => {
+  it('should be created', () => {
     expect(service).toBeTruthy();
-  }));
+  });
 });
